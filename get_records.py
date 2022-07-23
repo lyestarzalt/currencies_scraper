1f5fe7cc90b7646f29c98904c8effe67eaa6de37
Lyes <lyestarzalt@gmail.com>
2022-07-23 14:31:40 +0800

    code cleanup

--- get_records.py
+++ get_records.py
@@ -13,60 +13,37 @@ alger_timezone = pytz.timezone('Africa/Algiers')
 
 
 class GetCurrencies():
-  
+
   def __init__(self):
       self.password = {'afous': 'moh!12!'}
       self.url = 'http://www.forexalgerie.com/connect/updateExchange.php'
-  
+      self.buyDict = {}
+      self.sellDict = {}
+      self.returnedJson = {}
+      self.todayDate = datetime.today().strftime('%Y-%m-%d')
   def getdata(self):
     post = requests.post(self.url,self.password, verify=False).json()
-    post_json = post[0]
-    return post_json
-  
-  
+    self.returnedJson = post[0]
+    return self.returnedJson
+  def seperateValues(self):
+      for key , value in self.returnedJson.items():
+        if '_buy' in key:
+          self.buyDict[key[0:3]] = float(value)
+        elif '_sell' in key:
+          self.sellDict[key[0:3]] = float(value)
+      return [self.sellDict,self.buyDict]
+    
+    
 
 
 
 if __name__ == "__main__":
-
-  
-  
   #get the data from the website.
   todays_records = GetCurrencies()
   record_data=todays_records.getdata()
-  
-  
-  
-  document_name = record_data['record_no']
-  record_data.pop('create_date_time',None)
-  #convert the values 
-  for key, value in record_data.items():
-      if "_buy" or "_sell"  in key:
-          record_data[key] = float(value)
-          record_data[key] = float(value)
-  
-  
-  
-  buy_price ={}
-  sell_price ={}
-  #seprate the buy and sell values to different dictionaries.
-  for key , value in record_data.items():
-    if '_buy' in key:
-      buy_price[key[0:3]] = value
-    elif '_sell' in key:
-      sell_price[key[0:3]] = value
-
-  #TODO :convert the time to algerian time.
-  today = datetime.today().strftime('%Y-%m-%d')
 
   #upload the data to firebase
   cred = credentials.Certificate("exchange-dinar-key.json")
   firebase_admin.initialize_app(cred)
   db = firestore.client()
-  #db.collection(u'exchange-daily').document(str(today)).set({'buy': buy_price, 'sell': sell_price})
-
-  db.collection(u'today_price').document('sell').set(sell_price)
-  db.collection(u'today_price').document('buy').set(buy_price)
-
-
-  db.collection(u'history').document(str(today)).set(record_data)
+  db.collection(u'exchange-daily').document(str(todays_records.todayDate)).set({"anis":todays_records.seperateValues()})
