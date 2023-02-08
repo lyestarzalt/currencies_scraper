c7b320f6b6c21c57f1add0cb02ea7dbedbba282f
Lyes Tarzalt <lyestarzalt@gmail.com>
2023-02-08 14:01:39 +0800

    add lambda_handler

--- post_database.py
+++ post_database.py
@@ -5,29 +5,44 @@ from datetime import datetime
 from scraper import DinarScraper
 
 
-""" This script scrape the latest exchange rates for the Algerian Dinar
-    It compares the data from two sources and prints the latest one
-    
-    Im using it to upload the data to firestore database.
-"""
+def lambda_handler(event, context):
+
+    try:
+        sr = DinarScraper()
+
+        first_source = sr.get_forex_data()
+        second_source = sr.get_devise_dz_data()
+
+        # see who has the latest data
+        if first_source[0] > second_source[0]:
+            sell_dict = {
+                currency.name: currency.sell for currency in first_source[1]}
+            buy_dict = {
+                currency.name: currency.buy for currency in first_source[1]}
+        else:
+            sell_dict = {
+                currency.name: currency.sell for currency in second_source}
+            buy_dict = {
+                currency.name: currency.buy for currency in second_source}
+
+        # upload the data to firebase
+        cred = credentials.Certificate("exchange-dinar-key.json")
+        firebase_admin.initialize_app(cred)
+        db = firestore.client()
+        db.collection(u'exchange-daily').document(str(datetime.now().date())
+                                                  ).set({"anis": [sell_dict, buy_dict]})
+
+        return {
+            'statusCode': 200,
+            'body': 'Data updated successfully'
+        }
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode': 400,
+            'body': f'An error occured'
+        }
 
 
-sr = DinarScraper()
-
-first_source = sr.get_forex_data()
-second_source = sr.get_devise_dz_data()
-
-# see who has the latest data
-if first_source[0] > second_source[0]:
-    sellDict = {currency.name: currency.sell for currency in first_source[1]}
-    buyDict = {currency.name: currency.buy for currency in first_source[1]}
-else:
-    sellDict = {currency.name: currency.sell for currency in second_source}
-    buyDict = {currency.name: currency.buy for currency in second_source}
-
-# upload the data to firebase
-cred = credentials.Certificate("exchange-dinar-key.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-db.collection(u'exchange-daily').document(str(datetime)
-                                          ).set({"anis": [sellDict, buyDict]})
+if __name__ == "__main__":
+    lambda_handler(None, None)
