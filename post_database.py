79bf6fbef039c2313507ff512df9592858538313
Lyes Tarzalt <lyestarzalt@gmail.com>
2023-02-18 00:48:33 +0800

    add env var

--- post_database.py
+++ post_database.py
@@ -3,9 +3,10 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from datetime import datetime
 from scraper import DinarScraper
+from dotenv import load_dotenv
+import os
 
-
-def lambda_handler(event, context):
+def lambda_handler(event, context) -> dict:
 
     try:
         sr = DinarScraper()
@@ -26,7 +27,22 @@ def lambda_handler(event, context):
                 currency.name: currency.buy for currency in second_source}
 
         # upload the data to firebase
-        cred = credentials.Certificate("exchange-dinar-key.json")
+        
+       
+        keys = {
+            "type": "service_account",
+            "project_id": os.getenv("PROJECT_ID"),
+            "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+            "private_key": os.getenv("PRIVATE_KEY"),
+            "client_email": os.getenv("CLIENT_EMAIL"),
+            "client_id": os.getenv("CLIENT_ID"),
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL")
+        }
+
+        cred = credentials.Certificate(keys)
         firebase_admin.initialize_app(cred)
         db = firestore.client()
         db.collection(u'exchange-daily').document(str(datetime.now().date())
@@ -45,4 +61,6 @@ def lambda_handler(event, context):
 
 
 if __name__ == "__main__":
+    # Running locally
+    load_dotenv()
     lambda_handler(None, None)
