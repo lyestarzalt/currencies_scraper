8ab69dcb1d19ad5ef98a34b51fd912d9d05f4124
Lyes <lyes.trzlt@gmail.com>
2024-05-06 16:19:15 +0800

    remove time it

--- lambda_function.py
+++ lambda_function.py
@@ -50,55 +50,34 @@ def update_trend_data(currency, new_data_date, new_data_buy):
 
 def lambda_handler(event, context) -> dict:
     try:
-        start_time = time.time()
 
         # Initialize Scraper and Data Provider
-        init_start = time.time()
         scraper = DinarScraper()
         data_provider = CurrencyDataProvider('countries.json')
-        init_end = time.time()
-        print(f"Initialization Time: {init_end - init_start} seconds")
 
         # Fetch core currencies data
-        fetch_start = time.time()
         _, core_currencies_list = scraper.get_forex_data()
         core_currencies = {
             currency.currencyCode: currency for currency in core_currencies_list}
-        fetch_end = time.time()
-        print(f"Fetching Core Currencies Time: {
-              fetch_end - fetch_start} seconds")
-
         # Choose a base currency and create ExtraCurrencyManager
-        manager_start = time.time()
         base_currency = core_currencies.get('EUR', None)
         if not base_currency:
             raise ValueError("Base currency not found in core currencies")
         manager = ExtraCurrencyManager(
             base_currency, core_currencies, data_provider)
         non_core_currencies = manager.calculate_converted_currencies()
-        manager_end = time.time()
-        print(f"ExtraCurrencyManager Processing Time: {
-              manager_end - manager_start} seconds")
 
         # Merge core and non-core currencies
-        merge_start = time.time()
         all_currencies = {
             **{currency.currencyCode: currency for currency in non_core_currencies}, **core_currencies}
-        merge_end = time.time()
-        print(f"Merging Currencies Time: {merge_end - merge_start} seconds")
 
         # Construct the exchange data for Firestore
-        construct_start = time.time()
         exchange_data = {currency_code: {"currencyCode": currency.currencyCode, "name": currency.name,
                                          "symbol": currency.symbol, "flag": currency.flag, "buy": currency.buy,
                                          "sell": currency.sell, "date": currency.date, "is_core": currency.is_core}
                          for currency_code, currency in all_currencies.items()}
-        construct_end = time.time()
-        print(f"Constructing Data Time: {
-              construct_end - construct_start} seconds")
 
         # Upload the data to Firebase and update trends
-        firebase_start = time.time()
         current_date = datetime.now().date()
         db.collection(u'exchange-daily').document(str(current_date)).set(exchange_data)
         core_currencies = {currency.currencyCode: currency for currency in core_currencies_list}
@@ -112,12 +91,6 @@ def lambda_handler(event, context) -> dict:
         for lang in languages:
             send_localized_notification(lang, current_date)
 
-        firebase_end = time.time()
-        print(f"Firebase Upload and Trend Update Time: {
-              firebase_end - firebase_start} seconds")
-
-        total_end = time.time()
-        print(f"Total Execution Time: {total_end - start_time} seconds")
         return {'statusCode': 200, 'body': 'Data updated successfully'}
     except firebase_admin.exceptions.FirebaseError as fe:
         print(f"Firebase error: {fe}")
