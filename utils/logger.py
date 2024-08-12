f17c41cbd0123c2a227af354724dcbd1705f958a
Lyes <lyes.trzlt@gmail.com>
2024-08-12 12:19:54 +0800

    fix: duplicate logging handler

--- utils/logger.py
+++ utils/logger.py
@@ -4,19 +4,22 @@ from logging import Logger
 
 def get_logger(name: str) -> Logger:
     logger = logging.getLogger(name)
+    
+    if not logger.hasHandlers():
+        logger.setLevel(logging.DEBUG)
 
-    logger.setLevel(logging.DEBUG)
+        c_handler = logging.StreamHandler(sys.stdout)
+        c_handler.setLevel(logging.DEBUG)
 
-    c_handler = logging.StreamHandler(sys.stdout)  
-    c_handler.setLevel(logging.DEBUG) 
+        c_format = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        c_handler.setFormatter(c_format)
 
-    c_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    c_handler.setFormatter(c_format)
+        if hasattr(c_handler, 'setEncoding'):
+            c_handler.setEncoding('utf-8')
 
-    c_handler.setStream(sys.stdout)
-    if hasattr(c_handler, 'setEncoding'):
-        c_handler.setEncoding('utf-8')
-
-    logger.addHandler(c_handler)
+        logger.addHandler(c_handler)
 
     return logger
